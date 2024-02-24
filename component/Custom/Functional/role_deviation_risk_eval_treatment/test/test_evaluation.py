e5bec50662c0a5337db240372ab167fa27766e3b
Joe <joe@zauber.co.uk>
2024-02-24 22:49:39 +0000

    Evaluation tests

--- component/Custom/Functional/role_deviation_risk_eval_treatment/test/test_evaluation.py
+++ component/Custom/Functional/role_deviation_risk_eval_treatment/test/test_evaluation.py
@@ -31,3 +31,117 @@ class TestRiskEvaluation(CommonTest):
             evaluation.evaluate(risk_input, 97.6)
             p.assert_called_with(97.6, deviation)
             take_no_action.assert_called_with(risk_input, 97.6, treatment)
+
+    @patch.object(RiskTreatment, "inform")
+    def test_evaluation_surplus_inform(self, inform):
+        evaluation = RiskEvaluation()
+        risk_input = MagicMock()
+        deviation = DeviationType.SURPLUS
+        risk_input.type_of_deviation = deviation
+        treatment = MagicMock()
+        treatment.treatment = "Inform"
+        with patch(
+                RiskClassficiationEvalTreatmentLookup.get_treatment_for_risk_level,
+                return_value=treatment) as p:
+            evaluation.evaluate(risk_input, 97.6)
+            p.assert_called_with(97.6, deviation)
+            inform.assert_called_with(risk_input, 97.6, treatment)
+
+    @patch.object(RiskTreatment, "action")
+    def test_evaluation_surplus_action(self, action):
+        evaluation = RiskEvaluation()
+        risk_input = MagicMock()
+        deviation = DeviationType.SURPLUS
+        risk_input.type_of_deviation = deviation
+        treatment = MagicMock()
+        treatment.treatment = "Action"
+        with patch(
+                RiskClassficiationEvalTreatmentLookup.get_treatment_for_risk_level,
+                return_value=treatment) as p:
+            evaluation.evaluate(risk_input, 97.6)
+            p.assert_called_with(97.6, deviation)
+            action.assert_called_with(risk_input, 97.6, treatment)
+
+    @patch.object(RiskTreatment, "take_no_action")
+    @patch.object(RiskTreatment, "inform")
+    @patch.object(RiskTreatment, "action")
+    def test_evaluation_surplus_different_type(self, take_no_action, inform, action):
+        evaluation = RiskEvaluation()
+        risk_input = MagicMock()
+        deviation = DeviationType.SURPLUS
+        risk_input.type_of_deviation = deviation
+        treatment = MagicMock()
+        treatment.treatment = "Unknown"
+        with patch(
+                RiskClassficiationEvalTreatmentLookup.get_treatment_for_risk_level,
+                return_value=treatment) as p:
+            evaluation.evaluate(risk_input, 97.6)
+            p.assert_called_with(97.6, deviation)
+            take_no_action.assert_not_called()
+            inform.assert_not_called()
+            action.assert_not_called()
+
+    @patch.object(RiskTreatment, "take_no_action")
+    def test_evaluation_deficit_no_treatment(self, take_no_action):
+        evaluation = RiskEvaluation()
+        risk_input = MagicMock()
+        deviation = DeviationType.DEFICIT
+        risk_input.type_of_deviation = deviation
+        treatment = MagicMock()
+        treatment.treatment = "No Action"
+        with patch(
+                RiskClassficiationEvalTreatmentLookup.get_treatment_for_risk_level,
+                return_value=treatment) as p:
+            evaluation.evaluate(risk_input, 97.6)
+            p.assert_called_with(97.6, deviation)
+            take_no_action.assert_called_with(risk_input, 97.6, treatment)
+
+    @patch.object(RiskTreatment, "inform")
+    def test_evaluation_deficit_inform(self, inform):
+        evaluation = RiskEvaluation()
+        risk_input = MagicMock()
+        deviation = DeviationType.DEFICIT
+        risk_input.type_of_deviation = deviation
+        treatment = MagicMock()
+        treatment.treatment = "Inform"
+        with patch(
+                RiskClassficiationEvalTreatmentLookup.get_treatment_for_risk_level,
+                return_value=treatment) as p:
+            evaluation.evaluate(risk_input, 97.6)
+            p.assert_called_with(97.6, deviation)
+            inform.assert_called_with(risk_input, 97.6, treatment)
+
+    @patch.object(RiskTreatment, "action")
+    def test_evaluation_deficit_action(self, action):
+        evaluation = RiskEvaluation()
+        risk_input = MagicMock()
+        deviation = DeviationType.DEFICIT
+        risk_input.type_of_deviation = deviation
+        treatment = MagicMock()
+        treatment.treatment = "Action"
+        with patch(
+                RiskClassficiationEvalTreatmentLookup.get_treatment_for_risk_level,
+                return_value=treatment) as p:
+            evaluation.evaluate(risk_input, 97.6)
+            p.assert_called_with(97.6, deviation)
+            action.assert_called_with(risk_input, 97.6, treatment)
+
+    @patch.object(RiskTreatment, "take_no_action")
+    @patch.object(RiskTreatment, "inform")
+    @patch.object(RiskTreatment, "action")
+    def test_evaluation_deficit_different_type(self, take_no_action, inform,
+                                               action):
+        evaluation = RiskEvaluation()
+        risk_input = MagicMock()
+        deviation = DeviationType.DEFICIT
+        risk_input.type_of_deviation = deviation
+        treatment = MagicMock()
+        treatment.treatment = "Unknown"
+        with patch(
+                RiskClassficiationEvalTreatmentLookup.get_treatment_for_risk_level,
+                return_value=treatment) as p:
+            evaluation.evaluate(risk_input, 97.6)
+            p.assert_called_with(97.6, deviation)
+            take_no_action.assert_not_called()
+            inform.assert_not_called()
+            action.assert_not_called()
